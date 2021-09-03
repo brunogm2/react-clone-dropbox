ae16061c28421614ad5099f3e291b3509ef7e7cb
brunogm2 <brunobgm002@gmail.com>
2021-09-03 00:32:52 -0300

    UI Clone - Dropbox

---
+++ src/components/Section/index.tsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import { Container, Content, HeaderWrapper, Header, DropboxLogo } from './style';
+
+// import { Container } from './styles';
+
+interface Props {
+  variant: 'blue' | 'beige' | 'white' | 'black'
+  title: string;
+  description: string;
+}
+
+const Section: React.FC<Props> = ({variant, title, description}) => {
+
+  const buttonVariant = Math.round(Math.random());
+
+  function handleToggle() {
+    if(window.toggleActiveMenu) window.toggleActiveMenu();
+  }
+
+  return (
+    <Container className={variant}>
+      <HeaderWrapper>
+        <Header>
+          <h1>
+              <DropboxLogo/>
+              <span>Dropbox</span>
+          </h1>
+
+          <button onClick={handleToggle}>{buttonVariant == 0 ? 'Acessar' : 'Interagir'}</button>
+        </Header>
+      </HeaderWrapper>
+
+      <Content>
+        <h2>{title}</h2>
+        <p>{description}</p>
+      </Content>
+    </Container>
+  );
+}
+
+export default Section;
